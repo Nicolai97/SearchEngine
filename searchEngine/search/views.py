e655d4b2717642ce0aa8e72b3d8b7c5e672baf23
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2020-12-21 19:30:57 +0100

    remove print

--- searchEngine/search/views.py
+++ searchEngine/search/views.py
@@ -4,6 +4,5 @@ def search_bar(request):
   return render(request, 'search/search_bar.html')
 
 def make_search(request):
-  print(request.POST.get('search_text'))
   return render(request, 'search/search_results.html', {'search_text': request.POST.get('search_text')})
 
