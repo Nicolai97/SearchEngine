b6a48669df71fd78496b8dcb3db69cd49c1878c0
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2021-01-09 11:48:37 +0100

    map evaluation algorithm

---
+++ searchEngine/search/core/evaluations/map.py
@@ -0,0 +1,18 @@
+# precision = N relevant doc / N retrived doc
+# recall = N relevant doc / N total doc
+
+# precision = N intersezione tra relevant e retrived / N retrived doc 
+# recall = N intersezione tra relevant e retrived / N relevant doc
+
+
+# creare un dictionary con i 10 risultati rilevanti per ogni ricerca (per ogni ricerca salviamo il titolo e il link di google)
+# ricerche da considerare
+# apple
+# DNA
+# Hollywood
+# Computer Programming
+# Financial meldown
+# Triple Cross
+# Mean Average Precision
+# Much ado about nothing
+# calcolare i valori di precision, average precision e recall su 10 risultati
